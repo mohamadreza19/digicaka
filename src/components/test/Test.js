a2ad30622a7614efcb5c0dd2c4d6eb96d7991384
unknown <mrzar1380@gmail.com>
2022-12-24 13:38:39 +0330

    ss

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,18 +1,1 @@
-import {
-  Magnifier,
-  GlassMagnifier,
-  SideBySideMagnifier,
-  PictureInPictureMagnifier,
-  MOUSE_ACTIVATION,
-  TOUCH_ACTIVATION,
-} from "@ricarso/react-image-magnifiers";
 
-export default function Test() {
-  return (
-    <SideBySideMagnifier
-      className="w-25"
-      imageSrc="https://s3.amazonaws.com/images.seroundtable.com/google-stan-dinosaur-service-dino-1657283267.jpg"
-      largeImageSrc="https://s3.amazonaws.com/images.seroundtable.com/google-stan-dinosaur-service-dino-1657283267.jpg"
-    />
-  );
-}
